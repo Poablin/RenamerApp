f118a3956a8b3fdacd3f7d360a219aae56641a73
Poablin <65161335+Poablin@users.noreply.github.com>
2020-11-26 13:05:04 +0100

    La til basic hvor mange items er selected

--- RenamerApp/EditorWindow.cs
+++ RenamerApp/EditorWindow.cs
@@ -10,8 +10,10 @@ namespace RenamerApp
     {
         private ListBox InformationList { get; }
         private TextBox OutputDirectoryInputBox { get; }
+        private CheckBox CopyCheckBox { get; }
         private CheckBox UpperCaseCheckBox { get; }
         private CheckBox TrimCheckBox { get; }
+        private EditorButton SelectButton { get; }
         private string[] FilePaths { get; set; }
 
         public EditorWindow()
@@ -19,19 +21,21 @@ namespace RenamerApp
             var grid = new Grid();
             InformationList = new EditorInformationList();
             OutputDirectoryInputBox = new EditorTextBox("Output Path", 200, 100, 70);
+            CopyCheckBox = new EditorCheckBox(200, 20, 350, 10, "Copy");
             UpperCaseCheckBox = new EditorCheckBox(200, 20, 350, 40, "Uppercase");
             TrimCheckBox = new EditorCheckBox(200, 20, 350, 70, "Trim");
             var startButton = new EditorButton(0, 0, "Start");
-            var selectButton = new EditorButton(0, 50, "Select");
+            SelectButton = new EditorButton(0, 50, "Select");
             startButton.Click += StartOperation;
-            selectButton.Click += SelectFiles;
+            SelectButton.Click += SelectFiles;
 
             grid.Children.Add(InformationList);
+            grid.Children.Add(CopyCheckBox);
             grid.Children.Add(UpperCaseCheckBox);
             grid.Children.Add(TrimCheckBox);
             grid.Children.Add(OutputDirectoryInputBox);
             grid.Children.Add(startButton);
-            grid.Children.Add(selectButton);
+            grid.Children.Add(SelectButton);
 
             Content = grid;
         }
@@ -58,16 +62,24 @@ namespace RenamerApp
                     //name = name.Replace("  ", " ");
                     if (TrimCheckBox.IsChecked == true) name = name.Trim();
                     name = UpperCaseCheckBox.IsChecked == true ? name.Substring(0, 1).ToUpper() + name[1..] : name.Substring(0, 1).ToLower() + name[1..];
-                    InformationList.Items.Add($"Renamed \"{oldn}\" to: \"{name}{exte}\" {(outputDirectory == "" ? "" : $"and moved file to {outputDirectory}")}");
                     //Her bestemmer man hvor det skal outputtes til
-                    File.Move($"{file}", $"{(outputDirectory == "" ? dire : outputDirectory)}\\{name}{exte}");
+                    if (CopyCheckBox.IsChecked == true)
+                    {
+                        File.Copy($"{file}", $"{(outputDirectory == "" ? dire : outputDirectory)}\\{name}{exte}");
+                        InformationList.Items.Add($"Renamed \"{oldn}\" to \"{name}{exte}\" {(outputDirectory == "" ? "" : $"and copied file to {outputDirectory}")}");
+                    }
+                    else 
+                    { 
+                        File.Move($"{file}", $"{(outputDirectory == "" ? dire : outputDirectory)}\\{name}{exte}");
+                        InformationList.Items.Add($"Renamed \"{oldn}\" to \"{name}{exte}\" {(outputDirectory == "" ? "" : $"and moved file to {outputDirectory}")}");
+                    }
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                InformationList.Items.Add("Error renaming files");
+                InformationList.Items.Add(ex);
             }
-            finally { FilePaths = null; }
+            finally { FilePaths = null; SelectButton.Content = "Select"; }
 
         }
         private void SelectFiles(object sender, RoutedEventArgs e)
@@ -78,6 +90,7 @@ namespace RenamerApp
             if (openFileDialog.ShowDialog() == true)
             {
                 FilePaths = openFileDialog.FileNames;
+                SelectButton.Content = $"Select ({FilePaths.Length})";
             }
         }
     }
